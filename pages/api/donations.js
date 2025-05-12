480cd547d5745e0d375075337956d8ebc642590c
nellz16 <149651360+nellz16@users.noreply.github.com>
2025-05-13 00:26:11 +0700

    Update donations.js

--- pages/api/donations.js
+++ pages/api/donations.js
@@ -1,6 +1,6 @@
-import { getDonations } from '../../server/db';
+import { getStats } from '../../server/db';
 
 export default async function handler(req, res) {
-  const { total, leaderboard } = await getDonations();
+  const { total, leaderboard } = await getStats();
   res.status(200).json({ total, leaderboard });
 }
