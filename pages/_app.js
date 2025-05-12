806f9cb5c2f64ab4d708b96c38864520dc119ff8
nellz16 <149651360+nellz16@users.noreply.github.com>
2025-05-13 01:26:28 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
-import '../styles/globals.css';
+import 'styles/globals.css';
 import { DonationProvider } from '../context/DonationContext';
 import Head from 'next/head';
-
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <DonationProvider>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Head><meta name="viewport" content="width=device-width, initial-scale=1"/></Head>
       <Component {...pageProps} />
     </DonationProvider>
   );
 }
-export default MyApp;
